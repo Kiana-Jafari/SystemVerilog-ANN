5613f845c2dffed91b9d2df4b705ccee5ec69d30
Kiana <kiana.jafari.ai@gmail.com>
2026-06-19 18:19:53 +0330

    Add neuron architecture

---
+++ SV ANN/Combinational/src/neuron.sv
@@ -0,0 +1,29 @@
+module neuron #(
+    parameter int N_INPUTS = 2,
+                INPUT_WIDTH = 16,
+                DATA_WIDTH = 16,
+                ACC_WIDTH = 32
+) (
+
+    input logic signed [INPUT_WIDTH-1:0] x[N_INPUTS],
+    input logic signed [DATA_WIDTH-1:0] w[N_INPUTS],
+    input logic signed [DATA_WIDTH-1:0] b,
+
+    output logic signed [ACC_WIDTH-1:0] z
+);
+
+    always_comb begin : linear_operation
+        
+        z = 0;
+
+        for (int i = 0; i < N_INPUTS; i++) begin : MAC
+            
+            z += (x[i] * w[i]) >>> 12;
+        
+        end
+
+        z += {{(ACC_WIDTH-DATA_WIDTH){b[DATA_WIDTH-1]}}, b};
+
+    end
+    
+endmodule
